6d418ac366ac072fa32003c2da2792d1b98e5f09
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-06-22 14:59:59 +0530

    d

--- 02Intermediate/myObjects.js
+++ 02Intermediate/myObjects.js
@@ -1,3 +1,21 @@
 let myYoutubeVideo = {
-    title: 'Loops in javascript'
+    title: 'Loops in javascript',
+    videoLength: '15',
+    videoCreator: 'John Doe',
+    videoDescription: 'this is a video description'
 }
+
+
+console.log(myYoutubeVideo)
+
+console.log(`Hey new video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`)
+
+
+// //Assignment
+// let myCourse = {
+//     name: 'John Doe', 
+//     price: "$100", 
+//     description: "Introductory course on Javascript",
+// }
+
+// console.log(`Hey, checkout the new course by ${myCourse.name} on ${myCourse.description}, priced at ${myCourse.price}`)
---
+++ 02Intermediate/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let myYoutubeVideo = {
+    title: 'Loops in javascript',
+    videoLength: '15',
+    videoCreator: 'John Doe',
+    videoDescription: 'this is a video description'
+}
+
+
+console.log(myYoutubeVideo)
+
+console.log(`Hey new video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`)
