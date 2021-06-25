2c9e8e74e17aa0e7241601ec1bb3ba9b6e8c2f2b
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-06-25 14:38:12 +0530

    d

---
+++ 02Intermediate/equals.js
@@ -0,0 +1,24 @@
+// console.log(2 === 2.0)
+
+// console.log ('' === '')
+
+// let myVar = ''
+// let myVarTwo = ''
+
+// console.log (myVar === myVarTwo)
+
+// let myVar = {}  // {} is object
+// let myVarTwo = {}   // {} is object
+
+// console.log (myVar === myVarTwo)
+
+
+let myVar = {}  // {} is object
+let myVarTwo = myVar   // {} is object
+
+console.log (myVar === myVarTwo)
+
+console.log({} === {})
+
+// == equals checks if values are equal
+// === equals checks if two objects in memory are exactly the same
