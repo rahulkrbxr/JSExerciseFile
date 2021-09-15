5bfcd50a67ed6c002e90b9782d2db789df2d3a97
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-09-15 23:55:44 +0530

    d

---
+++ Tricky/strings.js
@@ -0,0 +1,12 @@
+var s1 = 4 + 'js';
+console.log(s1);
+
+var s2 = "rahul" + 2;
+console.log(s2);
+
+var x = 1 + 3 + "magic";    // 4magic
+var y = "magic" + 1 + 3;    // magic13
+console.log(x + "\n" + y);
+
+var s3 = (2*3) + "upGrad" + 2 + 3 + "upGrad" + 2 + (3 - 2);
+console.log(s3);
