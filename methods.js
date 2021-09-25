ece66cd6987e8f1e5937c5e715952555c3654426
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-09-25 11:59:58 +0530

    d

---
+++ methods.js
@@ -0,0 +1,10 @@
+// Method
+// method is a function that is assignet to a key in an object
+
+var user = {
+    name: 'Rahul',
+    greet: function() {
+        console.log("Hello User");
+    }
+}
+user.greet();
